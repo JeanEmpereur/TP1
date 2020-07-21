1eb9a8fd7f276c5e4cb728d7bc37a4854b037472
Empereur.j <jean.empereur@outlook.fr>
2020-07-21 09:03:09 +0200

    Initialisation
    Pour le front, la page connection + inscription marche au petit poil.
    La page de visualisation marche normalement

---
+++ tp1-back/src/api/controllers/userController.js
@@ -0,0 +1,66 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/userModel');
+
+exports.get_all_user = (req, res) => {
+  User.find({}, (error, users) => {
+    if(error){
+      res.status(500);
+      console.log(error);
+      res.json({message: "Erreur serveur."});
+    }
+    else{
+      res.status(200);
+      res.json(users);
+    }
+  })
+};
+
+exports.user_register = (req, res) => {
+  let new_user = new User(req.body);
+
+  new_user.save()
+  .then(user => {
+    res.status(201);
+    res.json(user);
+  })
+  .catch(error => {
+    res.status(500);
+    console.log(error);
+    res.json({message: "Erreur serveur."})
+  })
+}
+
+exports.user_login = (req, res) => {
+  let {body} = req;
+  // let body = req.body
+
+  User.findOne({email: body.email})
+  .then(user => {
+    console.log(body.email);
+    if(user.password === body.password){
+      let userData = {
+        email: user.email
+      }
+      jwt.sign({userData}, process.env.JWT_KEY, {expiresIn: '30 days'}, (error, token) => {
+        if(error){
+          res.status(500);
+          console.log(error);
+          res.json({message: "Erreur serveur."});
+        }
+        else {
+          res.json({token});
+        }
+      })
+    }
+    else{
+      res.status(500);
+      res.json({message: "Erreur serveur."})
+    }
+  })
+  .catch(error => {
+    res.status(500);
+    console.log(error);
+    res.json({message: "Erreur serveur."})
+  })
+}
---
+++ tp1-back/src/api/routes/userRoute.js
@@ -0,0 +1,10 @@
+module.exports = (server) => {
+  const userController = require('../controllers/userController');
+
+  server.post('/users/register', userController.user_register)
+
+  server.post('/users/login', userController.user_login)
+
+  server.get('/users', userController.get_all_user)
+
+}
